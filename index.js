1c4b9e3431d7f49bee30390f4263f11c4ccfcc2b
lytvynenko98a <93118800+lytvynenko98a@users.noreply.github.com>
2022-11-03 16:32:53 +0200

    Fix index.js not showing images

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ function JobBar(){
             <div id="content">
                 <img id="hospital-image" alt="hospital image" src="./export/Hospitalimage.svg"></img>
                 <div id="content-text-margin">
-                    <a href="next/src/App.tsx" id="job-ad-text"><span>Arbeitsmediziner/-in / Betriebsmediziner/-in (m/w/d) oder einen Arzt/eine Ärztin (m/w/d) für die Weiterbildung zum Facharzt/ zur Fachärztin für Arbeitsmedizin (m/w/d)</span></a>
+                    <a href="./next/src/App.tsx" id="job-ad-text"><span>Arbeitsmediziner/-in / Betriebsmediziner/-in (m/w/d) oder einen Arzt/eine Ärztin (m/w/d) für die Weiterbildung zum Facharzt/ zur Fachärztin für Arbeitsmedizin (m/w/d)</span></a>
                     <span id="hospital-name">
                     Department name •  Allgemeines Krankenhaus der Stadt Wien - AKH
                     </span>
                     <span id="location">
-                        <img alt="icon location" src="/export/Iconlocation.svg"></img>
+                        <img alt="icon location" src="./export/Iconlocation.svg"></img>
                         <span>Vienna, Austria</span>
                     </span>
                 </div>
-                <img alt="star rating" src="/export/5starrating.svg" id="star-rating"></img>
-                <img alt="save to" src="/export/Frame.svg" id="frame"></img>
+                <img alt="star rating" src="./export/5starrating.svg" id="star-rating"></img>
+                <img alt="save to" src="./export/Frame.svg" id="frame"></img>
             </div>
         </div>
     )    
@@ -45,11 +45,11 @@ function FreeJobAds() {
 function PageNumbers() {
     return (
         <div id="page">
-            <div id="left-arrow"><img alt="left arrow" src="/export/Arrow.svg"></img></div>
-            <div id="separator1"><img alt="separator1" src="/export/Separator.svg"></img></div>
-            <div id="pages-numbers"><img alt="numbers" src="/export/Pagesnumbers.svg"></img></div>
-            <div id="separator2"><img alt="separator2" src="/export/Separator.svg"></img></div>
-            <div id="right-arrow"><img alt="right arrow" src="/export/Arrow.svg"></img></div>
+            <div id="left-arrow"><img alt="left arrow" src="./export/Arrow.svg"></img></div>
+            <div id="separator1"><img alt="separator1" src="./export/Separator.svg"></img></div>
+            <div id="pages-numbers"><img alt="numbers" src="./export/Pagesnumbers.svg"></img></div>
+            <div id="separator2"><img alt="separator2" src="./export/Separator.svg"></img></div>
+            <div id="right-arrow"><img alt="right arrow" src="./export/Arrow.svg"></img></div>
         </div>
     )
 }
